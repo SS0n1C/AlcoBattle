866ab9fc084b04e6e10c97e167c2c28fad764562
ss0n1c <lls.s0n1cll@gamil.com>
2023-11-14 22:49:26 +0200

    Add beer table in store

--- src/store/modules/beertable.js
+++ src/store/modules/beertable.js
@@ -1,12 +1,17 @@
 export default{
     state: {
         beerdesk:[
-            {id:0},
-            {id:1},
-            {id:2},
-            {id:3},
-            {id:4},
-            {id:5},
+            {id:0, name: "Grimbergen Blanche",type:"ligth", alco:"6%",country:"Poland", design:{Vit:7, Evg:7},soft:{vit:8,evg:9},taste:{vit:8,evg:8}},
+            {id:1, name: "Sitnan",type:"ligth", alco:"5%",country:"Slovakia ", design:{Vit:6, Evg:6},soft:{vit:5,evg:4},taste:{vit:5,evg:4}},
+            {id:2, name: "Mecklenburger",type:"wheat", alco:"5.1%",country:"German", design:{Vit:4, Evg:5},soft:{vit:8,evg:8},taste:{vit:9,evg:7}},
+            {id:3, name: "xibeca damm",type:"ligth", alco:"4.6%",country:"Spain", design:{Vit:6, Evg:6},soft:{vit:6,evg:7},taste:{vit:7,evg:7}},
+            {id:4, name: "Royal Witbier",type:"ligth", alco:"5.2%",country:"Denmark", design:{Vit:5, Evg:6},soft:{vit:10,evg:9},taste:{vit:8,evg:8}},
+            {id:5, name: "Zubr",type:"ligth", alco:"4.1%",country:"Czech", design:{Vit:4, Evg:5},soft:{vit:6,evg:6},taste:{vit:5,evg:6}},
+            {id:6, name: "Karlova",type:"ligth", alco:"4%",country:"Czech", design:{Vit:5, Evg:6},soft:{vit:7,evg:6},taste:{vit:7,evg:6}},
+            {id:7, name: "Diesel tradici",type:"ligth", alco:"6%",country:"Lithuania", design:{Vit:7, Evg:7},soft:{vit:8,evg:8},taste:{vit:3,evg:5}},
+            {id:8, name: "Diesel dank",type:"dark", alco:"6%",country:"Lithuania", design:{Vit:8, Evg:7},soft:{vit:10,evg:9},taste:{vit:7,evg:6}},
+            {id:9, name: "Diesel strong",type:"ligth", alco:"7.4%",country:"Lithuania", design:{Vit:8, Evg:8},soft:{vit:6,evg:7},taste:{vit:5,evg:7}},
+            {id:10, name: "Pivna Dizhka",type:"dark", alco:"4.2%",country:"Ukrain", design:{Vit:8, Evg:9},soft:{vit:9,evg:9},taste:{vit:8,evg:8}},
         ]
     },
     getters: {
